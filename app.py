1f12434d4fba9d6032f78e1dc5a6027810ebdced
titan <titanicking24@gmail.com>
2025-05-25 23:19:34 +0530

    App template created

---
+++ app.py
@@ -0,0 +1,19 @@
+def main():
+    # print("Hi there")
+    # pdf to image
+
+    # Image pre process
+
+    # Text extraction with paddle
+
+    # Convert to structured json
+
+    # Deviation calculation flag
+
+    # Log events
+
+    # Give output in excell
+    pass
+
+
+main()
